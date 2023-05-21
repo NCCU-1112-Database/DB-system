a07ae91cdd5e2cb74eff561a833c1fb64dc56b5d
cloudy9982 <108703022@g.nccu.edu.tw>
2023-05-21 21:14:00 +0800

    MVC

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, Blueprint
+from flask_moment import Moment
+from datetime import datetime
+import sqlalchemy as db
+from sqlalchemy import func
+import math
+from view.member_register import member_app
+
+
+app = Flask(__name__)
+moment = Moment(app)
+app.register_blueprint(member_app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           page_header="index",
+                           current_time=datetime.utcnow())
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
